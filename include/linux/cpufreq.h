6d6ffd2092e9bd2ab2c3bcb68be0c0be0785c65b
tjarnold <tjarnold@seahawksystems.com>
2013-08-05 18:02:56 -0400

    Add Lionheart Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,6 +378,10 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_lionheart)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionheart)
+
 #endif
 
 
